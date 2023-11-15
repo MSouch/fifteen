9ae6f7f8b4cc98df2279b5b951bf22f81bbd1660
MSouch <msouch3@gmail.com>
2023-11-15 15:06:19 -0700

    Three classes created working code with signout method

---
+++ library.py
@@ -0,0 +1,14 @@
+class Library:
+    def __init__(self, city):
+        self.city = city  
+    def signOutABook(self, book, person):
+        pass
+
+class Book:
+    def __init__(self,title):
+        self.title = title
+
+class Bookworm:
+    def __init__(self,name):
+        self.name = name
+        
---
+++ main.py
@@ -0,0 +1,7 @@
+import library
+
+# sign out a book 
+calgaryLibrary = library.Library("Calgary")
+book =library.Book("philosophers stone")
+munir = library.Bookworm()
+calgaryLibrary.signOutABook(book)
